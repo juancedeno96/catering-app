b0e10e8609acdd91e1f178aaf316af33171b2281
juancedeno96 <cedenojj7@gmail.com>
2021-06-29 17:38:38 -0600

    added appointlet link

---
+++ src/Components/Navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+// import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="NavBar">
+      <nav className="nav">
+        <div className="schedule-button">
+          <button>
+            <a
+              href="https://juliescateringservice.appointlet.com/s/catering-service
+"
+              target="_blank"
+            >
+              Hire for Event
+            </a>
+          </button>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
