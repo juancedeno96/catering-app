84b572b3f1ca321fc7e4c1c34d37c1e1e629503c
juancedeno96 <cedenojj7@gmail.com>
2021-08-11 21:45:13 -0600

    Added font-awesome tag

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,21 +1,22 @@
 import React from "react";
-// import { NavLink } from "react-router-dom";
+import '../styles/_navbar.scss'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+
 
 const Navbar = () => {
   return (
     <div className="NavBar">
       <nav className="nav">
-        <div className="schedule-button">
-          <button>
-            <a
-              href="https://juliescateringservice.appointlet.com/s/catering-service
-"
-              target="_blank"
-            >
+          <button className="schedule-button">
+            <a 
+              href="https://juliescateringservice.appointlet.com/s/catering-service" target="_blank" rel='noreferrer'>
               Hire for Event
             </a>
           </button>
-        </div>
+          <a href="https://www.instagram.com/lajamadjulieta/" target="_blank" rel='noreferrer'>
+          <FontAwesomeIcon icon={faInstagram} className="icon_ig" />
+          </a>
       </nav>
     </div>
   );
