a66525103da2b735c5d6fedc71cd635e56d2d21a
juancedeno96 <cedenojj7@gmail.com>
2021-08-11 21:44:46 -0600

    removed logo link

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
 
@@ -6,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <Navbar/>
-     
+      
     </div>
   );
 }
