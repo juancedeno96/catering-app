862f273dd8e66188668c4066cdaf16f0681fe92e
juancedeno96 <cedenojj7@gmail.com>
2021-08-16 22:59:55 -0600

    implemented routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Navbar from './Components/Navbar';
+import routes from './routes'
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      
+      {routes}
     </div>
   );
 }
