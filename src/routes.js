a4d1d34f69c2062aa5061416defc2409d2e65720
juancedeno96 <cedenojj7@gmail.com>
2021-08-18 21:52:45 -0600

    Added other components

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "./Components/Home";
+import About from './Components/About'
+import Services from "./Components/Services";
+import Contact from "./Components/Contact";
 export default (
   <Switch>
-    <Route path="/" component={Home} />
-    {/* <Route path = '/about' component={About}/> */}
-    {/* <Route path = '/services' component={Services}/> */}
-    {/* <Route path='/contact' component={Contact}/> */}
+    <Route path="/" exact component={Home} />
+    <Route path = '/about' component={About}/>
+    <Route path = '/services' component={Services}/>
+    <Route path='/contact' component={Contact}/>
   </Switch>
 );
