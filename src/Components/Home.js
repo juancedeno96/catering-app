4c45e12d2a013ca9420415a20ff1d86fa1218c04
juancedeno96 <cedenojj7@gmail.com>
2021-08-19 23:06:19 -0600

    added img

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,46 @@
 import React from "react";
+import '../styles/home.css'
 
 const Home = () => {
-  return <div className="Home">
-      This is home
-  </div>;
+  return <div id="container">
+  <header>
+  </header>
+  
+  {/* <!-- Each image is 350px by 233px --> */}
+  <div class="photobanner">
+    
+    <img class="first" src="https://c2.staticflickr.com/9/8454/28951308921_6589e3f1d3_z.jpg" alt="placeholder"/>
+<img src="https://c3.staticflickr.com/9/8716/28407134794_bef4e3b216_z.jpg" alt="placeholder"/>
+
+<img src="https://c2.staticflickr.com/9/8334/28951307401_d8a7c852e9_z.jpg" alt="placeholder"/>
+
+<img src="https://c1.staticflickr.com/9/8190/28407132824_fdf7bfe154_z.jpg" alt="placeholder"/>
+
+<img src="https://c2.staticflickr.com/9/8210/28951305801_bfe7c0b346_z.jpg" alt="placeholder"/>
+
+<img src="https://c1.staticflickr.com/9/8716/28407131024_91c2626d3c_z.jpg" alt="placeholder"/>
+
+<img src="https://c2.staticflickr.com/9/8592/28951304321_a01fb808fb_z.jpg" alt="placeholder"/>
+
+<img src="https://c1.staticflickr.com/9/8869/28407129424_54ea0f3d5a_z.jpg" alt="placeholder"/>
+    <img  src="https://c2.staticflickr.com/9/8454/28951308921_6589e3f1d3_z.jpg" alt="placeholder"/>
+<img src="https://c3.staticflickr.com/9/8716/28407134794_bef4e3b216_z.jpg" alt="placeholder"/>
+
+<img src="https://c2.staticflickr.com/9/8334/28951307401_d8a7c852e9_z.jpg" alt="placeholder"/>
+
+<img src="https://c1.staticflickr.com/9/8190/28407132824_fdf7bfe154_z.jpg" alt="placeholder"/>
+
+<img src="https://c2.staticflickr.com/9/8210/28951305801_bfe7c0b346_z.jpg" alt="placeholder"/>
+
+<img src="https://c1.staticflickr.com/9/8716/28407131024_91c2626d3c_z.jpg" alt="placeholder"/>
+
+<img src="https://c2.staticflickr.com/9/8592/28951304321_a01fb808fb_z.jpg" alt="placeholder"/>
+
+<img src="https://c1.staticflickr.com/9/8869/28407129424_54ea0f3d5a_z.jpg" alt="placeholder"/>
+
+
+
+      </div>
+</div>
 }
 export default Home;
