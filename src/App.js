fb3ee42a01eaccd23f51f16419035d6bc81aea09
juancedeno96 <cedenojj7@gmail.com>
2021-08-21 22:24:09 -0600

    imported footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
 import routes from './routes';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Navbar/>
       {routes}
+      <Footer/>
     </div>
   );
 }
