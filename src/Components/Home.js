9f011eccfacde9b687c9d6a5d612afd81bc39bb7
juancedeno96 <cedenojj7@gmail.com>
2021-08-25 22:06:53 -0600

    updated

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "../styles/_home.scss";
+import verde from "../images/verde-bowls.jpg"
 
 const Home = () => {
   return (
@@ -11,59 +12,21 @@ const Home = () => {
       <div class="photobanner">
         <img
           class="first"
-          src="https://c2.staticflickr.com/9/8454/28951308921_6589e3f1d3_z.jpg"
+          src={verde}
           alt="placeholder"
         />
         <img
-          src="https://c3.staticflickr.com/9/8716/28407134794_bef4e3b216_z.jpg"
-          alt="placeholder"
-        />
-
-        <img
-          src="https://c2.staticflickr.com/9/8334/28951307401_d8a7c852e9_z.jpg"
+          src={verde}
           alt="placeholder"
         />
 
         <img
-          src="https://c1.staticflickr.com/9/8190/28407132824_fdf7bfe154_z.jpg"
+          src={verde}
           alt="placeholder"
         />
 
         <img
-          src="https://c2.staticflickr.com/9/8210/28951305801_bfe7c0b346_z.jpg"
-          alt="placeholder"
-        />
-
-        <img
-          src="https://c1.staticflickr.com/9/8716/28407131024_91c2626d3c_z.jpg"
-          alt="placeholder"
-        />
-
-        <img
-          src="https://c2.staticflickr.com/9/8592/28951304321_a01fb808fb_z.jpg"
-          alt="placeholder"
-        />
-
-        <img
-          src="https://c1.staticflickr.com/9/8869/28407129424_54ea0f3d5a_z.jpg"
-          alt="placeholder"
-        />
-        <img
-          src="https://c2.staticflickr.com/9/8454/28951308921_6589e3f1d3_z.jpg"
-          alt="placeholder"
-        />
-        <img
-          src="https://c3.staticflickr.com/9/8716/28407134794_bef4e3b216_z.jpg"
-          alt="placeholder"
-        />
-
-        <img
-          src="https://c2.staticflickr.com/9/8334/28951307401_d8a7c852e9_z.jpg"
-          alt="placeholder"
-        />
-
-        <img
-          src="https://c1.staticflickr.com/9/8190/28407132824_fdf7bfe154_z.jpg"
+          src={verde}
           alt="placeholder"
         />
 
@@ -73,17 +36,55 @@ const Home = () => {
         />
 
         <img
-          src="https://c1.staticflickr.com/9/8716/28407131024_91c2626d3c_z.jpg"
+          src={verde}
           alt="placeholder"
         />
 
         <img
-          src="https://c2.staticflickr.com/9/8592/28951304321_a01fb808fb_z.jpg"
+          src={verde}
           alt="placeholder"
         />
 
         <img
-          src="https://c1.staticflickr.com/9/8869/28407129424_54ea0f3d5a_z.jpg"
+          src={verde}
+          alt="placeholder"
+        />
+        <img
+          src={verde}
+          alt="placeholder"
+        />
+        <img
+          src={verde}
+          alt="placeholder"
+        />
+
+        <img
+          src={verde}
+          alt="placeholder"
+        />
+
+        <img
+          src={verde}
+          alt="placeholder"
+        />
+
+        <img
+          src={verde}
+          alt="placeholder"
+        />
+
+        <img
+          src={verde}
+          alt="placeholder"
+        />
+
+        <img
+          src={verde}
+          alt="placeholder"
+        />
+
+        <img
+          src={verde}
           alt="placeholder"
         />
       </div>
