358ac2298b860a5cc48cc9c788ec68685791f5d5
juancedeno96 <cedenojj7@gmail.com>
2021-08-25 22:06:48 -0600

    updated

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import catering1 from '../images/catering1.jpg'
+import "../styles/_about.scss";
+
 
 const About = () => {
     return (
-        <div>
-            About
+        <div className="About">
+             <img src={catering1} alt = "dishes being displayed "/>
+             <h1 className="about_title">About Us!</h1>
+             <div className="about_description">
+             <p>
+       Prepare for Pokem! Make it Ipsum! To protect the world from devastation!
+        To unite all peoples within our nation! To denounce the evils of truth
+        and love! To extend our reach to the stars above! Pokem! Ipsum! Pokem
+        Ipsum, blast off at the speed of light! Surrender now or prepare to
+        fight! Pokem Ipsum! That's right!
+       </p>
+       <p>
+       Prepare for Pokem! Make it Ipsum! To protect the world from devastation!
+        To unite all peoples within our nation! To denounce the evils of truth
+        and love! To extend our reach to the stars above! Pokem! Ipsum! Pokem
+        Ipsum, blast off at the speed of light! Surrender now or prepare to
+        fight! Pokem Ipsum! That's right!
+       </p>
+       <p>
+       Prepare for Pokem! Make it Ipsum! To protect the world from devastation!
+        To unite all peoples within our nation! To denounce the evils of truth
+        and love! To extend our reach to the stars above! Pokem! Ipsum! Pokem
+        Ipsum, blast off at the speed of light! Surrender now or prepare to
+        fight! Pokem Ipsum! That's right!
+       </p>
+             </div>
         </div>
     )
 }
