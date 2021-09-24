4c19982e88b4829ea1bcbb99bfed225c8a19a35f
juancedeno96 <cedenojj7@gmail.com>
2021-09-23 18:03:49 -0600

    createdx

---
+++ src/Components/allServices.js
@@ -0,0 +1,12 @@
+import catering2 from '../images/catering-2resized.jpeg'
+
+const allServices = [
+    {
+        id:1,
+        image: catering2,
+        title: 'Example',
+
+    }
+]
+
+export default allServices
