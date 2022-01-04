a9e47982a2e344482ac23d9956f68f372e3917a5
juancedeno96 <cedenojj7@gmail.com>
2022-01-03 19:20:59 -0700

    imported images

--- src/Components/allServices.js
+++ src/Components/allServices.js
@@ -1,16 +1,31 @@
-import catering2 from '../images/catering-2resized.jpeg'
+import wedding from '../images/wedding.jpg';
+import workParty from '../images/work-party.jpg';
+import party from '../images/catering-party.jpg';
+import holiday from '../images/holiday-party.jpeg';
 
 const allServices = [
     {
         id:1,
-        image: catering2,
-        title: 'Example',
+        image: wedding,
+        title: 'Weddings',
 
     },
     {
         id:2,
-        image: catering2,
-        title: 'Example2',
+        image: party,
+        title: 'Parties',
+
+    },
+    {
+        id:3,
+        image: workParty,
+        title: 'Work',
+
+    },
+    {
+        id:4,
+        image: holiday,
+        title: 'Holiday Specials',
 
     }
 ]
